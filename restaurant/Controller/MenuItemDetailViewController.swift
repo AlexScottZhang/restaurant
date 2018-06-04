7c53427699d45ea469135237fd7a8cd4b2486fb0
alexscott <alexzhangshuo@163.com>
2018-06-04 18:31:12 +0800

    Add order button animation

--- restaurant/Controller/MenuItemDetailViewController.swift
+++ restaurant/Controller/MenuItemDetailViewController.swift
@@ -9,15 +9,41 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var orderButton: UIButton!
+    
     var menuItem: MenuItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(menuItem)
+        updateUI()
     }
 
-   
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.orderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.orderButton.transform = CGAffineTransform.identity
+        }
+//        UIView.animate(withDuration: 0.3, animations: {
+//            self.orderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+//        }) { (_) in
+//            UIView.animate(withDuration: 0.3, animations: {
+//                self.orderButton.transform = CGAffineTransform.identity
+//            })
+//        }
+    }
+    
+    
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        descriptionLabel.text = menuItem.description
+        orderButton.layer.cornerRadius = 5.0
+    }
     
 
 
