478f0e39b5ee2a988d09e535d3a40e5c17349a42
alexscott <alexzhangshuo@163.com>
2018-06-05 10:54:19 +0800

    add urlcache

--- restaurant/AppDelegate.swift
+++ restaurant/AppDelegate.swift
@@ -16,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //25M memory, 50M disk for urlcache
+        let temporaryDirectory = NSTemporaryDirectory()
+        let urlCache = URLCache(memoryCapacity: 25_000_000, diskCapacity: 50_000_000, diskPath: temporaryDirectory)
+        URLCache.shared = urlCache
+        
         return true
     }
 
